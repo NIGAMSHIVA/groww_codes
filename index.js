d548f69701a8a4bf7c4f53875ee321ae0e09c10f
NIGAMSHIVA <210105056@hbtu.ac.in>
2025-10-09 00:57:51 +0530

    code is

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // callback function--> How many time the button is clicked
 
 
-
+var i = 0;
 
 document.getElementById("clickMe").addEventListener("click", function xyz() {
-    console.log("button is Clicked")
+    console.log(i++);
 })
 
 
