a9be3178c937dfba76fd9c8040b01fbbf8715e84
NIGAMSHIVA <210105056@hbtu.ac.in>
2025-10-09 00:48:06 +0530

    Callback

---
+++ index.js
@@ -0,0 +1,12 @@
+// callback function--> How many time the button is clicked
+
+
+
+
+document.getElementById("clickMe").addEventListener("click", function xyz() {
+    console.log("button is Clicked")
+})
+
+
+
+
